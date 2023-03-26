d02d7b026b6e759725025636c942fd370b7bee77
SP1029 <106822488+SP1029@users.noreply.github.com>
2023-03-26 16:55:12 +0530

    Updated Not Found

--- client/src/Notfound.js
+++ client/src/Notfound.js
@@ -3,10 +3,7 @@ import logo from "./components/logo.png"
 
 const Notfound = () => {
   return (
-    <>
-      <img src={logo} height={"100vh"}></img>
       <div><h5>404. That’s an error.<br></br>The requested URL was not found on this server. That’s all we know.</h5></div>
-    </>
   )
 }
 
