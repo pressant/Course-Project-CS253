9160eafb5646eebeae8fe032c164f204edfbd997
Kartik <kartiksoni21@iitk.ac.in>
2023-03-26 12:35:55 +0530

    added

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -2,11 +2,10 @@ import React, { useState } from "react"
 import "./register.css"
 import axios from "axios"
 import { useHistory } from "react-router-dom"
-
 const Register = () => {
 
     const history = useHistory()
-
+   
     const [ user, setUser] = useState({
         name: "",
         email:"",
@@ -16,17 +15,24 @@ const Register = () => {
         password:"",
         reEnterPassword:""
     })
-
-    const handleChange = e => {
+        const handleChange = e => {
         const { name, value } = e.target
         setUser({
             ...user,
             [name]: value
         })
     }
+    const Change = e => {
+        const { value } = e.target
+        setUser({
+            ...user,
+            identity: value
+        })
+    }
 
     const register = () => {
         const { name, email,rollno,identity, password, reEnterPassword } = user
+        console.log(user);
         if( name && email && password&&rollno&&identity && (password === reEnterPassword)){
             axios.post("http://localhost:9002/register", user)
             .then( res => {
@@ -44,10 +50,17 @@ const Register = () => {
             {console.log("User", user)}
             <h1>Register</h1>
             <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
-            <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
+            <input type="email" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
             <input type="text" name="rollno" value={user.rollno} placeholder="Your Roll No" onChange={ handleChange }></input>
             <input type="text" name="bg" value={user.bg} placeholder="Your Bloood Group" onChange={ handleChange }></input>
-            <input type="text" name="identity" value={user.identity} placeholder="Your Role" onChange={ handleChange }></input>
+            <select placeholder="Identity" onChange={Change} >
+<option id="identity">Please Select your Identity</option>
+<option  value="student">Student</option>
+<option value="doctor">Doctor</option>
+<option  value="receptionist">Receptionist</option>
+<option  value="nurse">Nurse</option>
+<option  value="pharmacist">Pharmacist</option>
+</select>
             <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
             <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></input>
 
